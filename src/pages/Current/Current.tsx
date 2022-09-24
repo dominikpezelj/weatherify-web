c9ef5a21338aac157307dbbfb3b34e2b626f29c7
Dominik Pezelj <pezelj.dominik@gmail.com>
2022-09-24 14:14:02 +0200

    Separated types and interfaces in folders, Update project structure, Add Current components, Update Search - onInputChange, blurOnSelect, Add usePrepareWeatherData hook, Update Current.tsx

--- src/pages/Current/Current.tsx
+++ src/pages/Current/Current.tsx
@@ -1,12 +1,28 @@
-import { Container } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { Container, useTheme } from '@mui/material';
+import Skeleton from '@mui/material/Skeleton';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Header } from '../../components/Header/Header';
 import { useGetCoordinates } from '../../hooks/useGetCoordinates';
 import { FetchData } from '../../lib/FetchData';
+import { weatherActions } from '../../store/weather';
 import { CurrentFeed } from './CurrentFeed';
 
 export const Current = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const { primary, secondary, tertiary, textCards } = theme.colors;
+  const wetherIsFetching = useSelector((state: any) => state.weather.weatherIsFetching);
   const { isFetching, weatherIsFetching } = FetchData();
+  useEffect(() => {
+    console.log(wetherIsFetching);
+    if (isFetching || weatherIsFetching) {
+      dispatch(weatherActions.weatherIsLoading(true));
+    } else {
+      dispatch(weatherActions.weatherIsLoading(true));
+    }
+  }, [wetherIsFetching, isFetching, weatherIsFetching, dispatch]);
+
   const weather = useSelector((state: any) => state.weather.weatherCurrent);
   const stringify = JSON.stringify(weather);
   const coordinates = useGetCoordinates();
@@ -15,10 +31,11 @@ export const Current = () => {
     <div>
       <Header />
       <Container maxWidth={'lg'} sx={{ pt: '2rem' }}>
-        Test
+        {wetherIsFetching && <Skeleton variant="rounded" width={210} height={60} />}
+        {!wetherIsFetching && <CurrentFeed />}
         {/* {stringify} */}
         {latitude} lon: {longitude}
-        <CurrentFeed />
+        <Skeleton sx={{ bgcolor: secondary }} variant="rectangular" width={210} height={118} />
       </Container>
     </div>
   );
